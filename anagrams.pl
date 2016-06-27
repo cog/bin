fbaee4ab86f2a850697ec91f7cbf47f801aa9912
José Castro <cog@cpan.org>
2016-06-27 10:17:41 +0100

    added anagrams.pl

---
+++ anagrams.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# jac
+use strict;
+use warnings;
+
+# we calculate the permutations of the input parameter, remove the repeated ones
+for (sort ( u( map { join '' , @$_ } p( split // , shift ) ) ) )
+{
+  # and then we simply print each of them
+  print"$_\n"
+}
+
+# this removes repeated elements from a list
+sub u {
+  my %v;
+  grep { !$v{$_}++ } @_
+}
+
+# and this is a function that calculates permutations... it could be better...
+sub p {
+  @_?map{my$n=$_;map{[$_[$n],@$_]}p(@_[0..$n-1,$n+1..$#_])}(0..$#_):[]
+}
