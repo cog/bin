947a782933d46c98c3ac45337fae19ef3bb50a99
José Castro <cog@cpan.org>
2016-06-28 10:51:10 +0100

    added trim.pl

---
+++ trim.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -p
+# trim, by jac
+use strict;
+
+BEGIN { our $trim = shift || 80; }
+
+our $trim;
+s/(?<=.{$trim}).*//
+
+# to do:
+
+# special case of \t
+# option to trim only on spaces
