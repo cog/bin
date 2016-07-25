8514abdd53c5815b57bf9c18a6c056d7d445c2f7
José Castro <cog@cpan.org>
2016-07-25 10:50:45 +0100

    tidied up the code a bit

--- anagrams.pl
+++ anagrams.pl
@@ -4,19 +4,24 @@ use strict;
 use warnings;
 
 # we calculate the permutations of the input parameter, remove the repeated ones
-for (sort ( u( map { join '' , @$_ } p( split // , shift ) ) ) )
-{
-  # and then we simply print each of them
-  print"$_\n"
+for ( sort ( u( map { join '', @$_ } p( split //, shift ) ) ) ) {
+
+    # and then we simply print each of them
+    print "$_\n";
 }
 
 # this removes repeated elements from a list
 sub u {
-  my %v;
-  grep { !$v{$_}++ } @_
+    my %v;
+    grep { !$v{$_}++ } @_;
 }
 
 # and this is a function that calculates permutations... it could be better...
 sub p {
-  @_?map{my$n=$_;map{[$_[$n],@$_]}p(@_[0..$n-1,$n+1..$#_])}(0..$#_):[]
+    @_
+      ? map {
+        my $n = $_;
+        map { [ $_[$n], @$_ ] } p( @_[ 0 .. $n - 1, $n + 1 .. $#_ ] )
+      } ( 0 .. $#_ )
+      : [];
 }
